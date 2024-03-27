381a1c34ba7a1974bbcaba935d160cb4580949ab
zhangkaili <18844110157@163.com>
2024-03-27 11:53:42 +0800

    添加打包命令

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'zhangkaili2020',
+          name: 'my-electron-app'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
